6cff4c282eab1aa74445dbaf902c869f81a6e073
John Thehow <johnthehow@qq.com>
2022-04-06 06:32:34 +0800

    bugfix for pos_prob_viz.py

--- pos_probe_viz.py
+++ pos_probe_viz.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 	layer_means = []
 	for i in range(1,13):
 		plt.scatter(acc[acc.layer==i].layer,acc[acc.layer==i].acc,s=12)
-		layer_means.append(acc[acc.lay==i].mean().acc)
+		layer_means.append(acc[acc.layer==i].mean().acc)
 	plt.plot(range(1,13),layer_means,marker='^',label='layer mean accuracy')
 	plt.plot([1,12],[baseline,baseline],color='k',ls='--',linewidth=1)
 	plt.text(4,baseline-0.034,f'local majority baseline: {baseline}')
