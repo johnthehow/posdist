a89da7b142f1c73aa738851bcd329c952443da9e
John Thehow <johnthehow@qq.com>
2022-04-06 05:24:16 +0800

    modified pos_probe_viz

--- pos_probe_viz.py
+++ pos_probe_viz.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
 	for i in range(1,13):
 		plt.scatter(acc[acc.lay==i].lay,acc[acc.lay==i].acc,s=12)
 		layer_means.append(acc[acc.lay==i].mean().acc)
-	plt.plot(range(1,13),layer_means,marker='^')
+	plt.plot(range(1,13),layer_means,marker='^',label='layer mean accuracy')
 	plt.plot([1,12],[baseline,baseline],color='k',ls='--',linewidth=1)
 	plt.text(4,baseline-0.034,f'local majority baseline: {baseline}')
+	plt.legend()
