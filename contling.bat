892263ff8660ebc3d07462cbccf4f2af269d2f2c
John Thehow <johnthehow@qq.com>
2023-08-08 00:44:12 +0800

    regular

---
+++ contling.bat
@@ -0,0 +1,21 @@
+mkdir C:\Users\dell\Desktop\thehow\TASKS\POSDIST\CORPUS
+mkdir C:\Users\dell\Desktop\thehow\TASKS\POSDIST\ATTNROWLABS
+mkdir C:\Users\dell\Desktop\thehow\TASKS\POSDIST\ATTNROWLABS\12
+mkdir C:\Users\dell\Desktop\thehow\TASKS\POSDIST\ATTNROWLABS\16
+mkdir C:\Users\dell\Desktop\thehow\TASKS\POSDIST\ATTNROWLABS\20
+mkdir C:\Users\dell\Desktop\thehow\TASKS\POSDIST\ATTNROWLABS\24
+mkdir C:\Users\dell\Desktop\thehow\TASKS\POSDIST\ATTNROWLABS\28
+mkdir C:\Users\dell\Desktop\thehow\TASKS\POSDIST\ATTNROWLABS\32
+mkdir C:\Users\dell\Desktop\thehow\TASKS\POSDIST\ATTNROWLABS\36
+
+mkdir C:\Users\dell\Desktop\thehow\TASKS\POSDIST\VIS\0301
+mkdir C:\Users\dell\Desktop\thehow\TASKS\POSDIST\VIS\0310
+
+start "" python -m thehow.posdist.attn_rowpos.py 12 16000 C:/Users/dell/Desktop/thehow/TASKS/POSDIST/CORPUS/en_lepzig_all.pkl C:/Users/dell/Desktop/thehow/TASKS/POSDIST/ATTNROWLABS/12 "and"
+start "" python -m thehow.posdist.attn_rowpos.py 16 16000 C:/Users/dell/Desktop/thehow/TASKS/POSDIST/CORPUS/en_lepzig_all.pkl C:/Users/dell/Desktop/thehow/TASKS/POSDIST/ATTNROWLABS/16 "and"
+start "" python -m thehow.posdist.attn_rowpos.py 20 16000 C:/Users/dell/Desktop/thehow/TASKS/POSDIST/CORPUS/en_lepzig_all.pkl C:/Users/dell/Desktop/thehow/TASKS/POSDIST/ATTNROWLABS/20 "and"
+start "" python -m thehow.posdist.attn_rowpos.py 24 16000 C:/Users/dell/Desktop/thehow/TASKS/POSDIST/CORPUS/en_lepzig_all.pkl C:/Users/dell/Desktop/thehow/TASKS/POSDIST/ATTNROWLABS/24 "and"
+start "" python -m thehow.posdist.attn_rowpos.py 28 16000 C:/Users/dell/Desktop/thehow/TASKS/POSDIST/CORPUS/en_lepzig_all.pkl C:/Users/dell/Desktop/thehow/TASKS/POSDIST/ATTNROWLABS/28 "and"
+start "" python -m thehow.posdist.attn_rowpos.py 32 16000 C:/Users/dell/Desktop/thehow/TASKS/POSDIST/CORPUS/en_lepzig_all.pkl C:/Users/dell/Desktop/thehow/TASKS/POSDIST/ATTNROWLABS/32 "and"
+start "" python -m thehow.posdist.attn_rowpos.py 36 16000 C:/Users/dell/Desktop/thehow/TASKS/POSDIST/CORPUS/en_lepzig_all.pkl C:/Users/dell/Desktop/thehow/TASKS/POSDIST/ATTNROWLABS/36 "and"
+
